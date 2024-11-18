34bb1c5384fcbd3ca2ac279379f6f051a77e3ece
Panachai Bualoi <panachai.bual@kmutt.ac.th>
2024-11-19 01:10:38 +0700

    update

--- test_zoo.py
+++ test_zoo.py
@@ -9,6 +9,15 @@ class TestZoo(unittest.TestCase):
         self.assertEqual(self.zoo.get_ticket_price(5), 50)
        
     # Add your additional test cases here.
-
+    def test_none_age_ticket_price(self):
+        self.assertEqual(self.zoo.get_ticket_price(-2000), "No Way!!")
+    def test_child_ticket_price(self):
+        self.assertEqual(self.zoo.get_ticket_price(8), 50)
+    def test_teenager_price(self):
+        self.assertEqual(self.zoo.get_ticket_price(17), 100)
+    def test_adult_age_ticket_price(self):
+        self.assertEqual(self.zoo.get_ticket_price(27), 150)
+    def test_elderly_ticket_price(self):
+        self.assertEqual(self.zoo.get_ticket_price(67), 100)
 if __name__ == '__main__':
-    unittest.main()
+    unittest.main() 
